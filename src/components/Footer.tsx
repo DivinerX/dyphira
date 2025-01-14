67ffd570d8cf05f7db30bb150b1dbd1d07622990
DivinerX <loc_yan@outlook.com>
2025-01-14 17:42:08 +0900

    footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,16 +45,19 @@ interface SocialLinkProps {
 
 const SocialLink: FC<SocialLinkProps> = ({ icon, link, title }) => {
   return (
-    <div className="relative p-0 text-[#C8FFD3] border-[0.5px] border-[#C8FFD3]">
-    {/* Right top corner */}
-    <div className="absolute top-0 right-0 w-1 h-1 border-r-[0.5px] border-t-[0.5px] border-[#fff] z-10"></div>
-    {/* Left bottom corner */}
-    <div className="absolute bottom-0 left-0 w-1 h-1 border-l-[0.5px] border-b-[0.5px] border-[#fff] z-10"></div>
-    <Link to={link}>
-      <div className="p-[6px]">
-        <img src={icon} alt={title} className="h-[10px]" />
-      </div>
-    </Link>
-  </div>
+    <div className="relative p-0 overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full border-[0.5px] border-[#D9D9D9]"></div>
+      {/* Right top corner */}
+      <div className="absolute top-0 right-0 w-1 h-1 border-r-[0.5px] border-t-[0.5px] border-[#fff] z-30"></div>
+      {/* Left bottom corner */}
+      <div className="absolute bottom-0 left-0 w-1 h-1 border-l-[0.5px] border-b-[0.5px] border-[#fff] z-30"></div>
+      <div className="absolute -top-1 -left-1 w-2 h-2 rotate-45 border-[0.5px] border-[#D9D9D9] bg-[#0d191a] z-30"></div>
+      <div className="absolute -bottom-1 -right-1 w-2 h-2 rotate-45 border-[0.5px] border-[#D9D9D9] bg-[#0d191a] z-30"></div>
+      <Link to={link}>
+        <div className="p-[6px]">
+          <img src={icon} alt={title} className="h-[10px]" />
+        </div>
+      </Link>
+    </div>
   );
 };
